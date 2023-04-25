11597378b6b36c3e5924578487102c08cc8728b4
AaronBarcos <abarcoscaballero@gmail.com>
2023-04-25 22:32:54 +0200

    install next-auth

---
+++ src/components/Login.jsx
@@ -0,0 +1,9 @@
+function Login() {
+  return (
+    <div>
+        <form action=""></form>
+    </div>
+  )
+}
+
+export default Login
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -7,6 +8,13 @@ export default function Home() {
   return (
     <div>
       <h1>Next.js + Tailwind CSS</h1>
+      <h1>
+        Welcome to my personal project of social media app.
+      </h1>
+      <h3>
+        Please, login or register to start using it.
+      </h3>
+
     </div>
   )
 }
